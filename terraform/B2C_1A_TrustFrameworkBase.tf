c98a9b941da3dc9ba9d883a3ffc91daa435f069e
christi4n <christian@mydevfactory.net>
2022-09-15 03:04:24 +0200

    [TASK] Refactor

--- terraform/B2C_1A_TrustFrameworkBase.tf
+++ terraform/B2C_1A_TrustFrameworkBase.tf
@@ -1,6 +1,6 @@
-resource "azureadb2cief_trust_framework_policy" "B2C_1A_Example" {
-  name = "B2C_1A_Example"
-  policy = templatefile("B2C_1A_Example.tftpl.xml", {
+resource "azureadb2cief_trust_framework_policy" "B2C_1A_TrustFrameworkBase" {
+  name = "B2C_1A_TrustFrameworkBase"
+  policy = templatefile("B2C_1A_TrustFrameworkBase.tftpl.xml", {
     tenant_name                        = var.TENANT_NAME
     tenant_id                          = var.TENANT_ID
     B2C_1A_TokenSigningKeyContainer    = azureadb2cief_trust_framework_key_set.B2C_1A_TokenSigningKeyContainer.name
